84ce5b07cd0e9b39626ccbb4184820edba1a95c5
Bhavna Jain <jbhavna732@gmail.com>
2024-04-24 16:32:54 +0530

    chnages done

--- custom.js
+++ custom.js
@@ -1,24 +1,20 @@
 var qrInput = document.querySelector('.qr-input');
 var qrBtn = document.querySelector('.qrcode-btn');
 var qrcodeResult = document.getElementById("Qr_Code")
-var size = document.getElementById('size').value
+
 qrBtn.addEventListener('click',function(){
-    console.log(qrInput.value)
     qrcodeResult.innerHTML = ""
      var qrInputValue = qrInput.value;
-     new QRCode(qrcodeResult,qrInputValue
-//     {
-//         text : qrInputValue,
-//         width : size,
-//         height: size,
-//         colorDark : "#000000",
-//  colorLight : "#ffffff",
-//  correctLevel : QRCode.CorrectLevel.H
-//     })
-     )
-
-     qrcodeResult.style.marginTop = "20px"
-
-    
+     var size = document.getElementById('size').value
+     new QRCode(qrcodeResult,
+    {
+        text : qrInputValue,
+        width : size,
+        height: size,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    })
+     qrcodeResult.style.marginTop = "20px"    
 })
 
